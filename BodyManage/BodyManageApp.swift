e3259e16d54a02c680b68607dc70b34609df973b
mtkw0127 <maccho.chekera@gmail.com>
2024-01-20 23:16:50 +0900

    first commit

---
+++ BodyManage/BodyManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyManageApp.swift
+//  BodyManage
+//
+//  Created by 松川知憲 on 2024/01/20.
+//
+
+import SwiftUI
+
+@main
+struct BodyManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
